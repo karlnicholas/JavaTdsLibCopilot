4e393fdc5739bc58247f33b9096d62728ec220ed
Karl NIcholas <karl.nicholas@outlook.com>
2026-01-16 10:51:57 -0500

    Checkpoint commit

--- src/main/java/org/tdslib/javatdslib/Row.java
+++ src/main/java/org/tdslib/javatdslib/Row.java
@@ -2,11 +2,4 @@ package org.tdslib.javatdslib;
 
 import java.util.List;
 
-public record Row(List<Byte[]> data) {
-  <T> T get(String columnName, Class<T> type) {
-    return (T)null;
-  };
-  <T> T get(int columnIndex, Class<T> type) {
-    return (T)null;
-  };
-}
+public record Row(List<byte[]> data) {}
--- src/main/java/org/tdslib/javatdslib/RowMetadata.java
+++ src/main/java/org/tdslib/javatdslib/RowMetadata.java
@@ -4,15 +4,28 @@ import org.tdslib.javatdslib.tokens.colmetadata.ColMetaDataToken;
 import org.tdslib.javatdslib.tokens.colmetadata.ColumnMeta;
 
 import java.util.List;
+import java.util.Optional;
 
 public record RowMetadata(ColMetaDataToken colMetaData) {
-  ColumnMeta getColumnMetadata(String name) {
-    return colMetaData.getColumns().get(0);
-  };
-  ColumnMeta getColumnMetadata(int index) {
-    return colMetaData.getColumns().get(0);
+  public Optional<ColumnMeta> getColumnMetadata(String name) {
+    for( ColumnMeta column: colMetaData.getColumns()) {
+        if( name.equalsIgnoreCase( column.getName())) {
+            return Optional.of(column);
+        }
+    }
+    return Optional.empty();
   }
-  List<? extends ColumnMeta> getColumnMetadatas() {
+  public Optional<ColumnMeta> getColumnMetadata(int index) {
+    var columns = colMetaData.getColumns();
+
+    if (index >= 0 && index < columns.size()) {
+      // Use ofNullable in case the actual element is null
+      return Optional.ofNullable(columns.get(index));
+    } else {
+      return Optional.empty();
+    }
+  }
+  public List<? extends ColumnMeta> getColumnMetadatas() {
     return colMetaData.getColumns();
   };
 }
