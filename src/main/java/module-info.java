8d52c477a5855697d4cca757f580a2d2de986ac9
Karl Nicholas <karl.nicholas@outlook.com>
2026-03-04 18:00:17 -0500

    Refactoring to LOB streaming results.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,8 +6,6 @@ module org.tdslib.javatdslib {
 
   // 2. Export ONLY your clean public API
   exports org.tdslib.javatdslib.api;
-  exports org.tdslib.javatdslib.reactive;
-  exports org.tdslib.javatdslib.internal;
 
   // 3. Register the R2DBC Service Provider (replaces the META-INF/services file)
   provides io.r2dbc.spi.ConnectionFactoryProvider
