d4f08c146b41a288d8d40930c62b55e2b7d071bc
Karl Nicholas <karl.nicholas@outlook.com>
2026-03-30 19:13:11 -0400

    Refactoring general effort on frame parsing and LOB handling.

--- src/main/java/org/tdslib/javatdslib/api/package-info.java
+++ src/main/java/org/tdslib/javatdslib/api/package-info.java
@@ -2,8 +2,7 @@
  * The public-facing R2DBC SPI implementation for the TDS Java Library.
  *
  * <p>This package contains the primary entry points for users, including
- * {@link org.tdslib.javatdslib.impl.TdsConnectionFactory} and
- * {@link org.tdslib.javatdslib.impl.TdsConnection}.
+ * {@link org.tdslib.javatdslib.api.TdsLibOptions}.
  * All internal TDS protocol logic is encapsulated away from this package.
  */
 package org.tdslib.javatdslib.api;
